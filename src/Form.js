9e437c482dad7d4232bbbd2e34f26251aecce072
brandon wirz <bwirz75@gmail.com>
2023-04-17 17:47:21 -0600

    add favorite color field

--- src/Form.js
+++ src/Form.js
@@ -2,14 +2,21 @@ import React from "react";
 // import Todo from "./Todo";
 
 export default function Form() {
-  const [formData, setFormData] = React.useState({
-    firstName: "",
-    lastName: "",
-    email: "",
-    comments: "",
-    isFriendly: true,
-    favColor: "",
-  });
+  const [formData, setFormData] = React.useState([
+    { firstName: "" },
+    { lastName: "" },
+    { email: "" },
+    { comments: "" },
+    { isFriendly: true },
+    { favColor: "" },
+  ]);
+
+  //  firstName: "",
+  //   lastName: "",
+  //   email: "",
+  //   comments: "",
+  //   isFriendly: true,
+  //   favColor: "",
   const [messages, setMessages] = React.useState("");
   // console.log(formData.employment);
   // console.log(formData.favColor);
@@ -30,14 +37,28 @@ export default function Form() {
     event.preventDefault();
     // console.log(formData.firstName);
     // console.log(formData.lastName);
-    // console.log(formData);
+    console.log(formData);
     setMessages(
-      `Hey, ${formData.firstName}  ${formData.lastName}  ${formData.email}  ${formData.comments}`
+      `Hey, ${formData.firstName}  ${formData.lastName}  ${formData.email}  ${formData.comments} ${formData.favColor}`
     );
-
+    // const myData = messages.map((formDat) => {
+    //   return `<p>{formDat}</p>`;
+    // });
+    // console.log(myData);
+    // resetForm();
     // setMessages("");
     // event.target.reset();
   }
+  // function toggleShown() {
+  //   console.log(messages);
+  //   setMessages((prevMes) => !prevMes);
+  // }
+
+  // const resetForm = () => {
+  //   setMessages("");
+  //   // formData.lastName === "";
+  //   console.log("reset form........");
+  // };
 
   return (
     <>
@@ -47,28 +68,35 @@ export default function Form() {
           placeholder="First Name"
           onChange={handleChange}
           name="firstName"
-          value={formData.firstName}
+          // value={formData.firstName}
         />
         <input
           type="text"
           placeholder="Last Name"
           onChange={handleChange}
           name="lastName"
-          value={formData.lastName}
+          // value={formData.lastName}
         />
         <input
           type="text"
           placeholder="Email"
           onChange={handleChange}
           name="email"
-          value={formData.email}
+          // value={formData.email}
         />
         <textarea
           type="text"
           placeholder="Comments"
           onChange={handleChange}
           name="comments"
-          value={formData.comments}
+          // value={formData.comments}
+        />
+        <textarea
+          type="text"
+          placeholder="Favorite Color"
+          onChange={handleChange}
+          name="favColor"
+          // value={formData.comments}
         />
         <input
           type="checkbox"
@@ -88,6 +116,12 @@ export default function Form() {
       <div className="app">
         <div className="todo-list">
           <h2>{messages}</h2>
+
+          {/* <h2 onClick={toggleShown}>{messages}</h2> */}
+          {/* <p>{formData.firstName}</p>
+          <p>{formData.lastName}</p>
+          <p>{formData.email}</p>
+          <p>{formData.comments}</p> */}
         </div>
       </div>
     </>
